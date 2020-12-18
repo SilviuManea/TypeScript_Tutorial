eca9249375db693ca9b467228e92a726904a11b1
Silviu <silviu.manea.hamzau@outlook.com>
2020-12-18 13:14:35 +0100

    lesson-4

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,30 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-//character = 20; //->it will complain because it's not the same type.
-character = 'luigi';
-//age = 'yoshy'; //->it will complain because it's not the same type.
-age = 40;
-//isBlackBelt = 'yes' //->it will complain because it's not the same type.
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad'); // -->same type, will work
+//names.push(3); // -->different types
+//names[0] = 3; // -->different types
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+//numbers.push('silviu'); // -->different types
+//numbers.push[1] = 'silviu' // -->different types
+var mixed = ['ken', 4, 'chun-li', 8, 9]; //mixed type array
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+//objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-//console.log(circ('hello')); //it will complain since is requesting a number.
-console.log(circ(5)); // this one compiles ok
-//TypeScript Tutorial #3 - Type Basics
+//updating properties
+ninja.age = 40;
+ninja.name = 'ryu';
+//ninja.age = '30' // age has to be the same type as in the declaration
+//updating the object - we can only update the values of the different properties, but we can not add new ones or change their type
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
+//TypeScript Tutorial #4 - Objects & Arrays
--- sandbox.ts
+++ sandbox.ts
@@ -1,21 +1,41 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+//arrays
+let names = ['luigi','mario','yoshi'];
 
-//character = 20; //->it will complain because it's not the same type.
-character = 'luigi';
+names.push('toad'); // -->same type, will work
+//names.push(3); // -->different types
+//names[0] = 3; // -->different types
 
-//age = 'yoshy'; //->it will complain because it's not the same type.
-age = 40;
+let numbers = [10,20,30,40];
 
-//isBlackBelt = 'yes' //->it will complain because it's not the same type.
-isBlackBelt = true;
+numbers.push(25);
+//numbers.push('silviu'); // -->different types
+//numbers.push[1] = 'silviu' // -->different types
 
-const circ = (diameter:number) =>{ //:number forces the argument to be a number.
-    return diameter *Math.PI;
+let mixed = ['ken', 4 , 'chun-li', 8 , 9]; //mixed type array
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+//objects
+
+let ninja = { //also this has to be always of type object
+    name : 'mario', //the property type remains fixed
+    belt: 'black',
+    age:30
+};
+
+//updating properties
+ninja.age = 40;
+ninja.name = 'ryu';
+//ninja.age = '30' // age has to be the same type as in the declaration
+
+//updating the object - we can only update the values of the different properties, but we can not add new ones or change their type
+ninja = {
+    name : 'yoshi',
+    belt : 'orange',
+    age:40,
+    //skills: [] //we can not change the structura of the initial object (ninja)
 }
 
-//console.log(circ('hello')); //it will complain since is requesting a number.
-console.log(circ(5)); // this one compiles ok
-
-//TypeScript Tutorial #3 - Type Basics
+//TypeScript Tutorial #4 - Objects & Arrays
